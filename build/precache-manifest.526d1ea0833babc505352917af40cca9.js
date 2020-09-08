4f28e9b9119e2ad821c406ea3f7cf971df512380
David Gagnon <toogreen@15inch.lan>
2020-09-08 01:09:54 -0400

    added build

---
+++ build/precache-manifest.526d1ea0833babc505352917af40cca9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "735937a85f43184423145d333d35bee0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d5ab0e5212a4820264b",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f4f234c47dfca8fe5360",
+    "url": "/static/js/2.3ef2d018.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.3ef2d018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d5ab0e5212a4820264b",
+    "url": "/static/js/main.266a4e30.chunk.js"
+  },
+  {
+    "revision": "f3260a36ed25041221ee",
+    "url": "/static/js/runtime-main.b584fe3d.js"
+  }
+]);
